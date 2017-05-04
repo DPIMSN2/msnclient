1e0661e6300d557499827c3cd3ade684ce10900c
Kevin <jetten.kevin@gmail.com>
2017-05-04 16:00:42 +0200

    Added domain an created JMS consumer and dispatcher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import jms.JMSDispatcher;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by Kevin.
  */
 public class Main {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) {
+        JMSDispatcher.getInstance();
+        try {
+            JMSDispatcher.publishMessage(QUEUE_NAME,"consumer alles");
+            JMSDispatcher.closeDispatcher();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
 }
